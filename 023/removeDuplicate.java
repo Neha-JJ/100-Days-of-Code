cd492861cd28687ad737f64102b1e36c25be6266
Neha J J <nehajithendrian@gmail.com>
2024-10-23 22:59:31 +0530

    progress

--- 023/removeDuplicate.java
+++ 023/removeDuplicate.java
@@ -1,18 +1,23 @@
-if (nums.length == 0) {
-    return 0;
-}
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
 
-// i will keep track of the index of the last unique element found.
-int i = 0;
+        // i will keep track of the index of the last unique element found.
+        int i = 0;
 
+        
+        for (int j = 1; j < nums.length; j++) {
+            
+            if (nums[j] != nums[i]) {
+                i++; 
+                nums[i] = nums[j]; // Update nums[i] 
+            }
+        }
 
-for (int j = 1; j < nums.length; j++) {
-    
-    if (nums[j] != nums[i]) {
-        i++; 
-        nums[i] = nums[j]; // Update nums[i] 
+        // Return the number of unique elements.
+        // Since i is the index of the last unique element, the number of unique elements is i + 1.
+        return i + 1;
     }
 }
-
-
-return i + 1;
